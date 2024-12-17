38e1e7df8ef00c9eddae6700af7bc3dcc950729a
may07082004 <maymehenni1@gmail.com>
2024-12-17 22:56:49 +0100

    mon site web

---
+++ includes/form3.php
@@ -0,0 +1,53 @@
+<?php  
+session_start();
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $first_name = $_POST["fname"];
+    $last_name = $_POST["lname"];
+    $email = $_POST["email"];
+    $pwd = password_hash($_POST["pwd"], PASSWORD_DEFAULT); 
+    $date_naissance = $_POST["dob"];
+
+    try {
+        require_once "dbh.inc.php";
+        
+        // Vérifier si l'email existe déjà
+        $query = "SELECT email FROM users WHERE email = :email";
+        $stmt = $pdo->prepare($query);
+        $stmt->bindParam(":email", $email);
+        $stmt->execute();
+
+        if ($stmt->rowCount() > 0) {
+            // Si l'email existe déjà, rediriger vers la page de connexion
+            header("Location: http://localhost/web4/Projet%20webdev/signin.php?error=email_exists");
+            exit();
+        } else {
+            // Insérer les nouvelles données de l'utilisateur
+            $query = "INSERT INTO users (first_name, last_name, email, pwd, date_naissance) 
+                      VALUES (:first_name, :last_name, :email, :pwd, :date_naissance)";
+            
+            $stmt = $pdo->prepare($query);
+            
+            $stmt->bindParam(":first_name", $first_name);
+            $stmt->bindParam(":last_name", $last_name);
+            $stmt->bindParam(":email", $email);
+            $stmt->bindParam(":pwd", $pwd);
+            $stmt->bindParam(":date_naissance", $date_naissance);
+
+            $stmt->execute();
+            $_SESSION["first_name"] = $first_name;
+            $_SESSION["last_name"] = $last_name;
+            
+            $pdo = null;
+            $stmt = null;
+            
+            header("Location: http://localhost/web4/Projet%20webdev/mimi.php");
+            exit(); 
+        }
+    } catch(PDOException $e) {
+        die("Error: " . $e->getMessage());
+    }
+} else {
+    header("Location: http://localhost/web4/Projet%20webdev/mimi.php");
+    exit();
+}
+?>
---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+</head>
+<body>
+	<form method="post" action="includes/formhandler.php" >
+		<input type="text" name="name" placeholder="name" required>
+		<input type="text" name="email" placeholder="email" required>
+		<input type="password" name="pwd" placeholder="password" required>
+		<button type="submit">Sign Up</button>
+	</form>
+
+</body>
+</html>
